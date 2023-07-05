1eb69e18a2532a8a3932f7fdfa8f49cf08bde120
0x4D0x5A <T.AndersenXL@gmail.com>
2023-07-05 16:21:56 +0200

    printb

--- Chapter 16/printb/printb.asm
+++ Chapter 16/printb/printb.asm
@@ -16,55 +16,56 @@ main:
 
 printb:
 section .data
-    strspace    db "",32
-    strLen      db  63
-    str1        db "1"
-    str0        db "0"
+    strspace    db " ",0
+    strLen      dq  63
+    str1        db "1",0
+    str0        db "0",0
     fmtstr      db "%s"
 section .bss
 section .text
     push    rbp
     mov     rbp,rsp
-    
     mov     r12, 0
 
 Initialloop:
-    ; check if eighth bit, if yes, print space
-    xor     rax,rax      ; clear rax (used in IDIV)
-    xor     edx, edx     ; clear edx (used in IDIV, stores the remainder)  
-    mov     ax, 8
-    mov     ebx, r12d
-    inc     ebx         
+    ; check if end of string
+    cmp     r12, [strLen]
+    je      exit
+    ; check if eighth bit, if yes, print space 
+    cmp     r12d, 0     ; Avoid dividing with 0 
+    je      Printloop      
+    
+    xor     rax,rax     ; clear rax (used in IDIV)
+    xor     rdx, rdx    ; clear edx (used in IDIV, stores the remainder) 
+    mov     eax, r12d   ; r12 is the counter
+    mov     ebx, 8      ; TODO: It prints a space after every 7 bit it should be after every 8
     idiv    ebx         ; divide the contents of EDX:EAX by the contents of EBX. 
-                        ; TODO:  Doesnt work. Remainder will also be zero when deviding by 2 and 4 
     cmp     edx, 0x0    ; remainder is stored in edx
-    jne     Printloop
+    jne     Printloop   ; print 1 or 0
+    ; else print space
     push    rdi
     push    0x0         ; for stack alignment
-    mov	    rdi, fmtstr
-    mov     rsi, strspace
+    mov	    rdi, strspace
+    mov     rsi, fmtstr
     mov	    rax,0       ; no floating point
-    call    printf      ; crashes when r12 reaches 7?
-    add     rsp,8       ; skip the 0x0 that was pushed to the stack prior to function call
+    call    printf  
+    add     rsp,8       ; skip the 0x0 that was pushed for alignment
     pop     rdi
-    cmp     r12, strLen ; TODO; I dont think this works?
-    jz      exit
     inc     r12
     jmp     Initialloop
 
 Printloop:
     ; check if bit equals 1, then print 1 else print 0
     ; test ah, 1<<1
-    shr     rdi, 1  ; does this work?
-    jnc     write1  ; jump no carry (means the bit is zero)
+    shr     rdi, 1  
+    jnc     write1      ; jump no carry (means the bit is zero)
     push    rdi
     push    0x0
     mov	    rdi, fmtstr
-    mov     rsi, str1   ; TODO; prints 10?
+    mov     rsi, str1   
     mov	    rax, 0		
     call    printf
     add     rsp,8
-    pop     rdi
     pop     rdi
     inc     r12
     jmp     Initialloop
@@ -85,4 +86,3 @@ exit:
     mov 	rsp,rbp
     pop 	rbp
     ret
-
