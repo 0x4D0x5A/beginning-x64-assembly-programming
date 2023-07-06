3b5650b5b27a61b327266aff3d826aa6b2313552
0x4D0x5A <T.AndersenXL@gmail.com>
2023-07-06 16:58:55 +0200

    added comments to printb.c

--- Chapter 16/16 bits1/printb.c
+++ Chapter 16/16 bits1/printb.c
@@ -2,15 +2,15 @@
 
 #include <stdio.h>
 
-void printb(long long n){
+void printb(long long n){ // n = rdi
 	long long s,c;	
-	for (c = 63; c >= 0; c--)
+	for (c = 63; c >= 0; c--) // iterates 64 times, because its 64 bit
   	{
-		s = n >> c;
+		s = n >> c;	// 
 		// space after every 8th bit
- 		if ((c+1) % 8 == 0) printf(" ");
+ 		if ((c+1) % 8 == 0) printf(" "); // check if its the last bit in a byte (8th bit) if yes, print space
 
-		if (s & 1)
+		if (s & 1)	// if bit is 1 print 1 else print 0
       			printf("1");
     		else
       			printf("0");
